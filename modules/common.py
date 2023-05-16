17702ea134d95a9a6df0c04a2b7807a00da6f7d4
lockheeed <anonimus140119@gmail.com>
2023-05-16 23:22:30 +0300

    linux supporting

--- modules/common.py
+++ modules/common.py
@@ -1,16 +1,28 @@
 import ctypes
+import os
+import subprocess
 
 
-VERSION = '1.0.1'
-
-MB_ICONERROR: int = 0x10
-MB_ICONWARNING: int = 0x30
-MB_ICONINFORMATION: int = 0x40
+VERSION = '1.0.2'
 
 
-def MessageBox(title: str, body: str, msg_type: int):
-    ctypes.WinDLL('user32').MessageBoxW(0, body, title, msg_type)
+def NtMessageType(mtype: str):
+    return {'error': 0x10, 'warning': 0x30, 'info': 0x40}.get(mtype, 0)
+
+
+def CPMessageBox(title: str, body: str, mtype: str):
+    if os.name == 'nt': 
+        ctypes.WinDLL('user32').MessageBoxW(0, body, title, NtMessageType(mtype))
+    else:
+        subprocess.run(f"""zenity --{mtype} --title='{title}' --text='{body}'""", shell=True)
 
 
 def GetMonitorResolution(k: float):
-    return int(ctypes.windll.user32.GetSystemMetrics(0) * k), int(ctypes.windll.user32.GetSystemMetrics(1) * k)
+    if os.name == 'nt': 
+        return int(ctypes.windll.user32.GetSystemMetrics(0) * k), int(ctypes.windll.user32.GetSystemMetrics(1) * k)
+    else: 
+        res = subprocess.Popen('xrandr | grep "\*" | cut -d" " -f4', 
+                                shell=True, 
+                                stdout=subprocess.PIPE).communicate()[0].decode().strip().split('x')
+        
+        return int(res[0]), int(res[1])
